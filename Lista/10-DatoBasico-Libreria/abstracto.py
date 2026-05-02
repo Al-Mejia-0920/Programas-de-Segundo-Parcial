c753841dc8ae495747b408c5d670542a13162814
Al-Mejia-0920 <aldo.mejia0920@gmail.com>
2026-05-02 11:54:05 -0600

    Add files via upload

---
+++ Lista/10-DatoBasico-Libreria/abstracto.py
@@ -0,0 +1,15 @@
+class dadlista:
+    def __init__(self):
+        self.arreglo = []
+    
+    def agregar(self, n):
+        pass
+
+    def quitar(self):
+        pass
+
+    def mostrar(self):
+        pass
+    
+    def tamaño(self):
+        pass
---
+++ Lista/10.1NuevoDatoLibreria/abstracta.py
@@ -0,0 +1,12 @@
+class dadlista:
+    def __init__(self):
+        self.arreglo = []
+    
+    def agregar(self, n, e, p):
+        pass
+
+    def quitar(self):
+        pass
+
+    def mostrar(self):
+        pass
